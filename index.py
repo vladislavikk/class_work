62cf00cf65684953aa9283b5cb642c7598c1e8b2
vladislavikk <cva-2015@list.ru>
2024-02-12 16:06:13 +0400

    Тренировки в полёте / task 2

--- index.py
+++ index.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html', title='Домашняя страница')
 
 
+@app.route('/training/<prof>')
+def training(prof) -> str:
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
